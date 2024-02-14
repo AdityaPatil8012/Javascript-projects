14028d3a37d76f8841f05ee9cef4e0f3f3e7e531
Aditya Patil <aadityapatil8012@gmail.com>
2024-02-14 23:43:06 +0530

    added a scroll and localStorage to the todo-list

--- todo-list/app.js
+++ todo-list/app.js
@@ -1,79 +1,74 @@
 const inputText = document.querySelector("#inputTask");
 const btnAdd = document.querySelector(".add");
 const list = document.querySelector("ul");
-// for that first create a new li;
-const listItem = document.createElement("li");
-
-// I want to add eventlistener to the delete button in the li;
-// so let's first create a delete button;
-const deleteBtn = document.createElement("button");
-deleteBtn.innerText = "Delete";
-// deleteBtn.setAttribute("class", "Delete");
-deleteBtn.classList.add("Delete");
-
-
-// let's create a array so that we can store all the items we have added;
 const liItems = [];
 
-
-
-// using Add task button
 btnAdd.addEventListener("click", addTask);
 
-// using Enter keyword; 
-inputText.addEventListener("keypress", (event)=>{
-  if(event.key === "Enter"){
+inputText.addEventListener("keypress", event => {
+  if (event.key === "Enter") {
     addTask();
   }
 });
 
-function addTask(){
+function addTask() {
+  const newTask = inputText.value.trim();
 
-  // now get the value from the input;
-  const newTask = inputText.value.trim()
-  console.log(newTask);
+  if (newTask !== "") {
+    const listItem = document.createElement("li");
+    listItem.textContent = newTask;
 
-  // this will only works when input field contents some value;
-  if(inputText !== ""){
+    const deleteBtn = document.createElement("button");
+    deleteBtn.innerText = "Delete";
+    deleteBtn.classList.add("Delete");
+    deleteBtn.addEventListener("click", () => {
+      listItem.remove();
+      const index = liItems.indexOf(newTask); // Find index by task text
+      if (index !== -1) {
+        liItems.splice(index, 1);
+        saveTasksToLocalStorage(); 
+      }
+    });
 
-  // now add that value to the li in ul;
-  // now add input text to that li;
-  listItem.textContent = newTask;
+    listItem.appendChild(deleteBtn);
+    list.appendChild(listItem);
+    liItems.push(newTask); // Push the task text
 
-  // Adding a delete button to the list item;
-  const deleteBtn = document.createElement("button");
-  deleteBtn.innerText = "Delete";
-  // deleteBtn.setAttribute("class", "Delete");
-  deleteBtn.classList.add("Delete");
-  deleteBtn.addEventListener("click", deleteListItem);
-
-  // let's append this to listItem;
-  listItem.appendChild(deleteBtn);
-
-  // now,append that li to ul;
-  list.appendChild(listItem);
-
-  liItems.push(listItem);
-
-  // clear the input field;
-  inputText.value = "";
+    saveTasksToLocalStorage();
+    inputText.value = "";
   }
 }
 
+function saveTasksToLocalStorage() {
+  localStorage.setItem("tasks", JSON.stringify(liItems));
+}
 
-  // // now append delete button to this;
-  // const deleteBtn = document.createElement("button");
-  // listItem.setAttribute("class", "Delete");
+function loadTasksFromLocalStorage() {
+  const storedTasks = localStorage.getItem("tasks");
+  if (storedTasks) {
+    const tasks = JSON.parse(storedTasks);
+    tasks.forEach(task => {
+      const listItem = document.createElement("li");
+      listItem.textContent = task;
 
-  // // append delete to li; 
-  // listItem.append(deleteBtn);
+      const deleteBtn = document.createElement("button");
+      deleteBtn.innerText = "Delete";
+      deleteBtn.classList.add("Delete");
+      deleteBtn.addEventListener("click", () => {
+        listItem.remove();
+        const index = liItems.indexOf(task);
+        if (index !== -1) {
+          liItems.splice(index, 1);
+          saveTasksToLocalStorage(); 
+        }
+      });
 
-  function deleteListItem(){
-    listItem.remove();
-
-    // remove the deleted items from the litems array;
-    const index = liItems.indexOf(liItem);
-    if(index !== -1){
-      liItems.splice(index, 1);
-    }
+      listItem.appendChild(deleteBtn);
+      list.appendChild(listItem);
+      liItems.push(task);
+    });
   }
+}
+
+window.addEventListener("load", loadTasksFromLocalStorage);
+
